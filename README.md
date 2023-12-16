0f8f5b5374748255350fb6b0b4a1182f19f722d1
boldyshev <boldyshevb@gmail.com>
2023-12-16 08:55:54 +0400

    entrypoint uncomment

---
+++ README.md
@@ -0,0 +1,5 @@
+*** a. Metrics
+Accuracy, F1 and Jaccard Index
+To evaluate model a converted time intervals to boolean masks:
+If there is a video of 5 frames and the interval is [2, 3]
+the boolean mask is
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#python3 -m venv .venv
-#source .venv/bin/activate
-#pip install -r requirements.txt
+python3 -m venv .venv
+./.venv/bin/activate
+pip install -r requirements.txt
 
 python3 demo.py $1 $2 $3
